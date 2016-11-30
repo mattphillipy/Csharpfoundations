5e63b0c11f0867ba5a1854a9017f822836b935b1
Matt Phillipy <phillipymd@gmail.com>
2016-11-29 22:16:35 -0800

    Assignment 4 Looping Commit and push

---
+++ Assignment_4_Looping/Assignment_4_Looping/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Assignment4
+{
+    static void Main()
+    {
+
+        Console.Write("Enter a number: ");
+        int UserNumber = int.Parse(Console.ReadLine());
+
+        int Start = 1;
+
+        int factorial = 1;
+        for (int i = Start; i <= UserNumber; i++)
+        {
+            factorial *= Start;
+        }
+
+        while (Start <= UserNumber)
+        {
+            Console.WriteLine("{0}!      =       {1}", Start, factorial);
+            Start = Start + 1;
+            factorial *= Start;
+        }
+
+    }
+}
